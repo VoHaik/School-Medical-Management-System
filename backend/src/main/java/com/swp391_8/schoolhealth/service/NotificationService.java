ecea41263733cc26a3252cd7e4e436644a140e6d
Võ Đồng Đức Khải <khaiqn2707@gmail.com>
2025-06-13 11:36:39 +0700

    Delete backend directory

--- backend/src/main/java/com/swp391_8/schoolhealth/service/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.swp391_8.schoolhealth.service;
-
-import com.swp391_8.schoolhealth.dto.NotificationDTO;
-import java.util.List;
-
-public interface NotificationService {
-    List<NotificationDTO> getNotificationsByUserId(Long userId);
-    // Potentially add methods like markAsRead, createNotification, etc.
-}
